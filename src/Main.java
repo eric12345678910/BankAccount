781e6c0df93c1922b8b092548d2d6da5c33fcbf5
eric12345678910 <ericlaudrum@gmail.com>
2024-09-22 16:01:39 -0400

    Adjust question order in sign-in process

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,36 @@ public class Main {
 
         System.out.println("===========Bank Accounts of the Internet============");
 
-        // User Input
+        // User Input - Create Scanner
         Scanner scanner = new Scanner(System.in);
 
         // Existing or New User
-        System.out.println("Do you have an account with us?%nPlease respond y/n");
+        System.out.println("Do you have an account with us\nPlease respond y/n");
         String response = scanner.nextLine();
-        boolean existingUser ;
 
+        // Existing User
         if(response.equals("y")){
-            existingUser = true;
+            System.out.println("Sign in now!");
+            System.out.println("Please enter your account number:");
+            String accountNumber = scanner.nextLine();
+
         }
+        // Not a User
         else if(response.equals("n")){
-            existingUser = false;
+            System.out.println("Don't have an account? Create one now!");
+
+            System.out.println("Please enter your first name: ");
+            String firstName = scanner.nextLine();
+
+            System.out.println("Please enter your last name: ");
+            String lastName = scanner.nextLine();
+
         }
         else{
             System.out.println("Error: Please enter a valid option.");
             return;
         }
 
-        if(!existingUser) {
-            System.out.println("Don't have an account? Create one now!");
-        }
-
-        else {
-            System.out.println("Sign in now!");
-        }
-
-        System.out.println("Please enter your first name: ");
-        String firstName = scanner.nextLine();
-
-        System.out.println("Please enter your last name: ");
-        String lastName = scanner.nextLine();
 
         System.out.println("Please enter your 9-digit account number: ");
         String accountNumber = scanner.nextLine();
