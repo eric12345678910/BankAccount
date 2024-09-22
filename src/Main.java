d0d86812953fd49041eaad895ba372a862df2e0f
eric12345678910 <ericlaudrum@gmail.com>
2024-09-22 15:09:00 -0400

    Ask user if account exists and handle boolean result

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("===========Bank Accounts of the Internet============");
 
+        // User Input
         Scanner scanner = new Scanner(System.in);
+
+        // Existing or New User
+        System.out.println("Do you have an account with us?%nPlease respond y/n");
+        String response = scanner.nextLine();
+        boolean existingUser ;
+
+        if(response.equals("y")){
+            existingUser = true;
+        }
+        else if(response.equals("n")){
+            existingUser = false;
+        }
+        else{
+            System.out.println("Error: Please enter a valid option.");
+            return;
+        }
+
+        if(!existingUser) {
+            System.out.println("Don't have an account? Create one now!");
+        }
+
+        else {
+            System.out.println("Sign in now!");
+        }
 
         System.out.println("Please enter your first name: ");
         String firstName = scanner.nextLine();
@@ -23,6 +48,6 @@ public class Main {
 
         account.accountSummary();
 
-
+        scanner.close();
     }
 }
