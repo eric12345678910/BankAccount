4010101f8ba6bf41089ada0393fb9641d1571b22
eric12345678910 <ericlaudrum@gmail.com>
2024-09-22 13:32:10 -0400

    Create methods to retreive user info

--- src/Account.java
+++ src/Account.java
@@ -6,5 +6,26 @@ public class Account {
     private double accountBalance;
 
 
+    public Account(String firstName, String lastName, String accountId, double accountBalance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountId = accountId;
+        this.accountBalance = accountBalance;
+    }
+
+    // Create methods to instantiate variables
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getAccountId() {
+        return accountId;
+    }
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your first name: ");
+        String firstName = scanner.nextLine();
     }
 }
